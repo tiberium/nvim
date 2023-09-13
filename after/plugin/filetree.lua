e89271080075234aa16a1511b34ce2441bfe4824
Tomasz Skalniak <tomasz.skalniak@cdq.com>
2023-09-13 16:27:05 +0200

    feat: Add key mapping for Neotree toggling

--- after/plugin/filetree.lua
+++ after/plugin/filetree.lua
@@ -1,3 +1,9 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-require('neo-tree').setup {}
+local neo_tree = require('neo-tree')
+
+neo_tree.setup{}
+
+vim.keymap.set('n', '<leader><Tab>', function ()
+  return ":Neotree toggle" .. "<cr>"
+end, { expr = true, desc = '[Tab] Toggle Neotree'})
