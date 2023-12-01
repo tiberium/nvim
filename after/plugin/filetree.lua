c5a76a4555a5a320fcd170f058152365c712c0ae
tiberium <tomasz.ska88@gmail.com>
2023-12-01 18:35:22 +0100

    feat: show dotfiles in neotree

--- after/plugin/filetree.lua
+++ after/plugin/filetree.lua
@@ -2,7 +2,13 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 local neo_tree = require('neo-tree')
 
-neo_tree.setup{}
+neo_tree.setup({
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+    },
+  }
+})
 
 vim.keymap.set('n', '<c-e>', function ()
   return ":Neotree toggle" .. "<cr>"
