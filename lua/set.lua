b6bfa1f28346e0ac14830827cf30f45a0eff7a04
Tomasz Skalniak <tomasz.ska88@gmail.com>
2024-11-22 15:29:25 +0100

    build: Update some plugins to newer versions

--- lua/set.lua
+++ lua/set.lua
@@ -48,7 +48,7 @@ vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone,noselect"
+vim.o.completeopt = "menuone,noselect,noinsert,popup"
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
