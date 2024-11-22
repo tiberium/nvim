a6c1f6762ca6e2732ef89e858d75beced820d434
Tomasz Skalniak <tomasz.ska88@gmail.com>
2024-11-22 17:18:16 +0100

    feat: Add keybindings for copilot-chat & reformat oil plugin

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -8,7 +8,18 @@ return {
 	build = "make tiktoken", -- Only on MacOS or Linux
 	config = function()
 		require("CopilotChat").setup({
-			-- Options
+			vim.keymap.set("n", "mc", "<Cmd>CopilotChatToggle<CR>"),
+			vim.keymap.set("n", "mx", "<Cmd>CopilotChatStop<CR>"),
+			vim.keymap.set("n", "mR", "<Cmd>CopilotChatReset<CR>"),
+			vim.keymap.set("v", "me", "<Cmd>CopilotChatExplain<CR>"),
+			vim.keymap.set("v", "mr", "<Cmd>CopilotChatReview<CR>"),
+			vim.keymap.set("v", "mo", "<Cmd>CopilotChatOptimize<CR>"),
+			vim.keymap.set("v", "md", "<Cmd>CopilotChatDocs<CR>"),
+			vim.keymap.set("v", "mt", "<Cmd>CopilotChatTest<CR>"),
+			vim.keymap.set("n", "<leader>mc", function()
+				local actions = require("CopilotChat.actions")
+				require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+			end),
 		})
 	end,
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,24 +1,24 @@
 return {
-  "stevearc/oil.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("oil").setup({
-      columns = { "icon", "size" },
-      -- keymaps = {
-      --   ["<C-y>"] = false,
-      --   ["<M-y>"] = "actions.select_split",
-      -- },
-      preview = {
-        width = 0.8,
-      },
-      view_options = {
-        show_hidden = true,
-      },
-    })
+	"stevearc/oil.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("oil").setup({
+			columns = { "icon", "size" },
+			-- keymaps = {
+			--   ["<C-y>"] = false,
+			--   ["<M-y>"] = "actions.select_split",
+			-- },
+			preview = {
+				width = 0.8,
+			},
+			view_options = {
+				show_hidden = true,
+			},
+		})
 
-    vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "Open parent directory" })
-    vim.keymap.set("n", "<space>-", require("oil").toggle_float)
-  end,
+		vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+	end,
 }
